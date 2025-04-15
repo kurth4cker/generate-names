83b9ced4936f75fd72ff8fa20f65028fd1f1ff50
kurth4cker <kurth4cker@gmail.com>
2025-04-15 13:54:47 +0300

    main: add -n option for allowing name count

--- main.c
+++ main.c
@@ -1,16 +1,30 @@
 // SPDX-License-Identifier: ISC
 // SPDX-FileCopyrightText: 2025 kurth4cker <kurth4cker@gmail.com>
 
+#define _POSIX_C_SOURCE 2
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <errno.h>
+#include <unistd.h>
 
-const size_t WORD_NAME_MAX = 16;
+typedef struct {
+	size_t name_count;
+} Config;
 
-const char VOWS[] = "aeiou";
-const char CONS[] = "bcdfghjklmnpqrstvwxyz";
+static void *
+xcalloc(size_t count, size_t size)
+{
+	void *ptr = calloc(count, size);
+	if (ptr == NULL) {
+		fprintf(stderr, "could not allocate memory: %s", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	return ptr;
+}
 
 static int
 random_char(const char *chars)
@@ -24,13 +38,16 @@ random_lower(void)
 	return rand() % ('z'-'a') + 'a';
 }
 
-int
-main(void)
+static void
+print_names(Config cfg)
 {
+	const char VOWS[] = "aeiou";
+	const char CONS[] = "bcdfghjklmnpqrstvwxyz";
+
 	srand(time(NULL));
 
-	char *const name = calloc(WORD_NAME_MAX + 1, sizeof(char));
-	for (size_t name_count = 0; name_count < 16; name_count++) {
+	char *const name = xcalloc( + 1, sizeof(char));
+	while (cfg.name_count--) {
 		const size_t name_len = rand() % 5 + 3;
 		name[name_len] = '\0';
 		name[0] = random_lower();
@@ -46,4 +63,23 @@ main(void)
 		printf("%s\n", name);
 	}
 	free(name);
+}
+
+int
+main(int argc, char **argv)
+{
+	Config cfg = {
+		.name_count = 8,
+	};
+
+	int opt;
+	while ((opt = getopt(argc, argv, "n:")) != -1) {
+		switch (opt) {
+		case 'n':
+			sscanf(optarg, "%zu", &cfg.name_count);
+			break;
+		}
+	}
+
+	print_names(cfg);
 }
