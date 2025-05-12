10a3b8aa902f9de2811a88ff1d63f521efae5ea2
kurth4cker <kurth4cker@gmail.com>
2025-05-12 11:06:10 +0300

    build: detect C compiler via $CC

--- nob.c
+++ nob.c
@@ -10,10 +10,20 @@ typedef struct {
 } Config;
 
 static void
+cc(Nob_Cmd *cmd)
+{
+	const char *cc = getenv("CC");
+	if (cc == NULL) {
+		cc = "cc";
+	}
+	nob_cmd_append(cmd, cc);
+}
+
+static void
 build(Nob_Cmd *cmd)
 {
 	cmd->count = 0;
-	nob_cmd_append(cmd, "cc");
+	cc(cmd);
 	nob_cmd_append(cmd, "-std=c11");
 	nob_cmd_append(cmd, "-g");
 	nob_cmd_append(cmd, "-Wall", "-Wextra");
